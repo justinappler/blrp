cfdfb1847010c9351d48d12f6d8d5809c5fa84a2
Justin Appler <justin.appler@gmail.com>
2014-05-21 21:24:13 -0700

    MongoStore now uses the existing mongoose connection

--- app.js
+++ app.js
@@ -55,7 +55,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(session({ store: new MongoStore({db: 'blurp'}), secret: 'keyboard cat' }));
+app.use(session({
+  store: new MongoStore({db: 'blurp', mongoose_connection: mongoose.connection}), 
+  secret: 'keyboard cat'
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
